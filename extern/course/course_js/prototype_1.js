5721952231536b69c9d25f5e081b970cc4c916af
victorpalanciuc <palanciucvictor20@gmail.com>
2023-02-05 23:45:20 +0200

    Updates

---
+++ extern/course/course_js/prototype_1.js
@@ -0,0 +1,22 @@
+// Prototype = obiect care mareste intervalul de actiuni pentru obiecte sau clase.
+
+// Un obiect care poate exista la alte obiecte
+const person = new Object({
+  name: 'Maxim',
+  age: 25,
+  greet: function() {
+    console.log('Greet!');
+  }
+});
+
+// Initializeaza o noua functie in person.prototype
+Object.prototype.sayHello = function () {
+  console.log('Hello!')
+}
+
+// Creeaza obiecte noi. (ca parametru v-a fi prototip pentru obiectul creat)
+const lena = Object.create(person)
+lena.name = 'Elena';
+lena.sayHello();
+//
+// const str = new String('I am string');
